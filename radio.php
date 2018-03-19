4220ea84224dc8f9114f5240617467ed9e722f0f
xhrbtk <xhrbtk@gmail.com>
2018-03-19 11:42:20 +0800

    123

---
+++ radio.php
@@ -0,0 +1,284 @@
+{
+channels: [
+{
+name: "漫步春天",
+channel_id: "public_tuijian_spring",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_spring.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_spring.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_spring.jpg-small"
+},
+{
+name: "秋日私语",
+channel_id: "public_tuijian_autumn",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_autumn.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_autumn.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_autumn.jpg-small"
+},
+{
+name: "温暖冬日",
+channel_id: "public_tuijian_winter",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_winter.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_winter.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_winter.jpg-small"
+},
+{
+name: "热歌",
+channel_id: "public_tuijian_rege",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_rege.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_rege.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_rege.jpg-small"
+},
+{
+name: "KTV金曲",
+channel_id: "public_tuijian_ktv",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_ktv.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_ktv.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_ktv.jpg-small"
+},
+{
+name: "Billboard",
+channel_id: "public_tuijian_billboard",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_billboard.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_billboard.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_billboard.jpg-small"
+},
+{
+name: "成名曲",
+channel_id: "public_tuijian_chengmingqu",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_chengmingqu.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_chengmingqu.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_chengmingqu.jpg-small"
+},
+{
+name: "网络歌曲",
+channel_id: "public_tuijian_wangluo",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_wangluo.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_wangluo.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_wangluo.jpg-small"
+},
+{
+name: "开车",
+channel_id: "public_tuijian_kaiche",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_kaiche.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_kaiche.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_kaiche.jpg-small"
+},
+{
+name: "影视",
+channel_id: "public_tuijian_yingshi",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_yingshi.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_yingshi.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_yingshi.jpg-small"
+},
+{
+name: "随便听听",
+channel_id: "public_tuijian_suibiantingting",
+cover_big: "http://cloud.hunger-valley.com/music/public_tuijian_suibiantingting.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_tuijian_suibiantingting.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_tuijian_suibiantingting.jpg-small"
+},
+{
+name: "经典老歌",
+channel_id: "public_shiguang_jingdianlaoge",
+cover_big: "http://cloud.hunger-valley.com/music/public_shiguang_jingdianlaoge.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_shiguang_jingdianlaoge.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_shiguang_jingdianlaoge.jpg-small"
+},
+{
+name: "70后",
+channel_id: "public_shiguang_70hou",
+cover_big: "http://cloud.hunger-valley.com/music/public_shiguang_70hou.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_shiguang_70hou.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_shiguang_70hou.jpg-small"
+},
+{
+name: "80后",
+channel_id: "public_shiguang_80hou",
+cover_big: "http://cloud.hunger-valley.com/music/public_shiguang_80hou.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_shiguang_80hou.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_shiguang_80hou.jpg-small"
+},
+{
+name: "90后",
+channel_id: "public_shiguang_90hou",
+cover_big: "http://cloud.hunger-valley.com/music/public_shiguang_90hou.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_shiguang_90hou.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_shiguang_90hou.jpg-small"
+},
+{
+name: "火爆新歌",
+channel_id: "public_shiguang_xinge",
+cover_big: "http://cloud.hunger-valley.com/music/public_shiguang_xinge.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_shiguang_xinge.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_shiguang_xinge.jpg-small"
+},
+{
+name: "儿歌",
+channel_id: "public_shiguang_erge",
+cover_big: "http://cloud.hunger-valley.com/music/public_shiguang_erge.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_shiguang_erge.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_shiguang_erge.jpg-small"
+},
+{
+name: "旅行",
+channel_id: "public_shiguang_lvxing",
+cover_big: "http://cloud.hunger-valley.com/music/public_shiguang_lvxing.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_shiguang_lvxing.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_shiguang_lvxing.jpg-small"
+},
+{
+name: "夜店",
+channel_id: "public_shiguang_yedian",
+cover_big: "http://cloud.hunger-valley.com/music/public_shiguang_yedian.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_shiguang_yedian.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_shiguang_yedian.jpg-small"
+},
+{
+name: "流行",
+channel_id: "public_fengge_liuxing",
+cover_big: "http://cloud.hunger-valley.com/music/public_fengge_liuxing.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_fengge_liuxing.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_fengge_liuxing.jpg-small"
+},
+{
+name: "摇滚",
+channel_id: "public_fengge_yaogun",
+cover_big: "http://cloud.hunger-valley.com/music/public_fengge_yaogun.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_fengge_yaogun.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_fengge_yaogun.jpg-small"
+},
+{
+name: "民谣",
+channel_id: "public_fengge_minyao",
+cover_big: "http://cloud.hunger-valley.com/music/public_fengge_minyao.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_fengge_minyao.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_fengge_minyao.jpg-small"
+},
+{
+name: "轻音乐",
+channel_id: "public_fengge_qingyinyue",
+cover_big: "http://cloud.hunger-valley.com/music/public_fengge_qingyinyue.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_fengge_qingyinyue.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_fengge_qingyinyue.jpg-small"
+},
+{
+name: "小清新",
+channel_id: "public_fengge_xiaoqingxin",
+cover_big: "http://cloud.hunger-valley.com/music/public_fengge_xiaoqingxin.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_fengge_xiaoqingxin.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_fengge_xiaoqingxin.jpg-small"
+},
+{
+name: "中国风",
+channel_id: "public_fengge_zhongguofeng",
+cover_big: "http://cloud.hunger-valley.com/music/public_fengge_zhongguofeng.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_fengge_zhongguofeng.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_fengge_zhongguofeng.jpg-small"
+},
+{
+name: "DJ舞曲",
+channel_id: "public_fengge_dj",
+cover_big: "http://cloud.hunger-valley.com/music/public_fengge_dj.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_fengge_dj.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_fengge_dj.jpg-small"
+},
+{
+name: "电影",
+channel_id: "public_fengge_dianyingyuansheng",
+cover_big: "http://cloud.hunger-valley.com/music/public_fengge_dianyingyuansheng.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_fengge_dianyingyuansheng.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_fengge_dianyingyuansheng.jpg-small"
+},
+{
+name: "轻松假日",
+channel_id: "public_xinqing_qingsongjiari",
+cover_big: "http://cloud.hunger-valley.com/music/public_xinqing_qingsongjiari.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_xinqing_qingsongjiari.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_xinqing_qingsongjiari.jpg-small"
+},
+{
+name: "欢快旋律",
+channel_id: "public_xinqing_huankuai",
+cover_big: "http://cloud.hunger-valley.com/music/public_xinqing_huankuai.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_xinqing_huankuai.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_xinqing_huankuai.jpg-small"
+},
+{
+name: "甜蜜感受",
+channel_id: "public_xinqing_tianmi",
+cover_big: "http://cloud.hunger-valley.com/music/public_xinqing_tianmi.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_xinqing_tianmi.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_xinqing_tianmi.jpg-small"
+},
+{
+name: "寂寞",
+channel_id: "public_xinqing_jimo",
+cover_big: "http://cloud.hunger-valley.com/music/public_xinqing_jimo.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_xinqing_jimo.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_xinqing_jimo.jpg-small"
+},
+{
+name: "单身情歌",
+channel_id: "public_xinqing_qingge",
+cover_big: "http://cloud.hunger-valley.com/music/public_xinqing_qingge.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_xinqing_qingge.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_xinqing_qingge.jpg-small"
+},
+{
+name: "舒缓节奏",
+channel_id: "public_xinqing_shuhuan",
+cover_big: "http://cloud.hunger-valley.com/music/public_xinqing_shuhuan.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_xinqing_shuhuan.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_xinqing_shuhuan.jpg-small"
+},
+{
+name: "慵懒午后",
+channel_id: "public_xinqing_yonglanwuhou",
+cover_big: "http://cloud.hunger-valley.com/music/public_xinqing_yonglanwuhou.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_xinqing_yonglanwuhou.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_xinqing_yonglanwuhou.jpg-small"
+},
+{
+name: "伤感",
+channel_id: "public_xinqing_shanggan",
+cover_big: "http://cloud.hunger-valley.com/music/public_xinqing_shanggan.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_xinqing_shanggan.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_xinqing_shanggan.jpg-small"
+},
+{
+name: "华语",
+channel_id: "public_yuzhong_huayu",
+cover_big: "http://cloud.hunger-valley.com/music/public_yuzhong_huayu.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_yuzhong_huayu.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_yuzhong_huayu.jpg-small"
+},
+{
+name: "欧美",
+channel_id: "public_yuzhong_oumei",
+cover_big: "http://cloud.hunger-valley.com/music/public_yuzhong_oumei.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_yuzhong_oumei.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_yuzhong_oumei.jpg-small"
+},
+{
+name: "日语",
+channel_id: "public_yuzhong_riyu",
+cover_big: "http://cloud.hunger-valley.com/music/public_yuzhong_riyu.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_yuzhong_riyu.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_yuzhong_riyu.jpg-small"
+},
+{
+name: "韩语",
+channel_id: "public_yuzhong_hanyu",
+cover_big: "http://cloud.hunger-valley.com/music/public_yuzhong_hanyu.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_yuzhong_hanyu.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_yuzhong_hanyu.jpg-small"
+},
+{
+name: "粤语",
+channel_id: "public_yuzhong_yueyu",
+cover_big: "http://cloud.hunger-valley.com/music/public_yuzhong_yueyu.jpg-big",
+cover_middle: "http://cloud.hunger-valley.com/music/public_yuzhong_yueyu.jpg-middle",
+cover_small: "http://cloud.hunger-valley.com/music/public_yuzhong_yueyu.jpg-small"
+}
+]
+}
